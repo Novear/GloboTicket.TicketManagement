e42fac24c864cc408e01fc97cd6ec18570f8bf58
Novear Nabil <novear.nabil@softec.ai>
2022-05-16 16:35:34 +0200

    Fourth Push

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -1,15 +1,18 @@
 ﻿using FluentValidation;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
 using GloboTicket.TicketManagement.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandValidator:AbstractValidator<CreateEventCommand>
     {
+        private readonly IEventRepository _eventRepository;
         public CreateEventCommandValidator()
         {
             RuleFor(p => p.Name)
@@ -21,10 +24,19 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
                 .NotEmpty().WithMessage("{Property Name} is required.")
                 .NotNull()
                 .GreaterThan(DateTime.Now);
+
+            RuleFor(e => e)
+                .MustAsync(EventNameAndDateUnique)
+                .WithMessage("An event with the same name and date already exists.");
     
             RuleFor(p => p.Price)
                 .NotEmpty().WithMessage("{Property Name} is required.")
                 .GreaterThan(0);
+        }
+        private async Task<bool> EventNameAndDateUnique(CreateEventCommand e, CancellationToken token) 
+        {
+            return !(await _eventRepository.IsEventNameAndDateUnique(e.Name, e.Date));
+
         }
     }
 }
---
+++ GloboTicket.TicketManagement.Application/Responses/BaseResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Responses
+{
+    public class BaseResponse
+    {
+        public BaseResponse()
+        {
+            Success = true;
+        }
+
+        public BaseResponse(string message = null)
+        {
+            Success = false;
+            Message = message;
+        }
+
+        public BaseResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> ValidationErrors { get; set; }
+    }
+}
